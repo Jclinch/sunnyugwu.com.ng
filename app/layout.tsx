71cbbf3c24532b545a885233043d6178d757a9b2
Sunny Ugwu <sunnyugwu2011@gmail.com>
2024-10-25 01:32:34 +0100

    google tag added for analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import PreloaderWrapper from "@/components/PreloaderWrapper";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +27,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="google-tag-manager" // Added id here
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5VQWDPWM');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+          id="google-tag-manager-noscript" // Added id here
+          dangerouslySetInnerHTML={{
+            __html: `
+              <noscript>
+                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5VQWDPWM"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+              </noscript>
+            `,
+          }}
+        />
+        
         <PreloaderWrapper />
         {children}
       </body>
--- components/social-media.tsx
+++ components/social-media.tsx
@@ -7,7 +7,8 @@ import {
   IconBrandWhatsapp,
   IconBrandX,
 } from "@tabler/icons-react";
-import Head from "next/head";
+// import Head from "next/head";
+// import Script from "next/script";
 
 
 export function SocialMedia() {
@@ -54,22 +55,7 @@ export function SocialMedia() {
   ];
   return (
     <>
-    <Head>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(h,o,t,j,a,r){
-                 h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:5184105,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `,
-        }}
-      />
-    </Head>
+    
     <div className="flex items-center justify-center h-[35rem] w-full ">
       <FloatingDock
         mobileClassName="translate-y-20" //remove for production
